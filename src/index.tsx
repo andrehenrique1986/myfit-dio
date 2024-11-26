5c8649a6e8d5084f107e99aa76e498023fdcd631
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2024-11-26 16:25:33 -0300

    Criação das Pastas

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const Main: React.FC = () => {
+  return (
+    <View>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default Main;
